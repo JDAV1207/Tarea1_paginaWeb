c05cf9be289ffc756a48787d0e6015b34d15a5be
User <User@DESKTOP-7AA3MOI>
2024-06-11 01:03:35 -0600

    Creacion de pagina web / 5 pgs

---
+++ src/test/java/com/paginaWeb/PaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paginaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
